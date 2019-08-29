3d1f26010b18f6d624b030e309c273400d4ce4e0
Theo Wiik <theo.le.wiik@gmail.com>
2019-08-29 19:23:00 +0200

    updated player script to move correctly

--- src/actors/player/player.gd
+++ src/actors/player/player.gd
@@ -5,13 +5,17 @@ onready var camera = $Pivot/Camera
 var velocity = Vector3()
 var gravity = 6
 var jump_speed = 4
-var max_walking_speed = 80
+var max_walking_speed = 7
 var max_sprinting_speed = 4
 var mouse_sensitivy = 0.006
 
-func _process(delta):
+func _physics_process(delta):
+	move(delta)
+	print(velocity.length())
+
+func move(delta):
 	update_velocity(delta)
-	move_and_slide(velocity, Vector3.UP)
+	velocity = move_and_slide(velocity, Vector3.UP, true)
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -26,7 +30,7 @@ func update_velocity(delta):
 	velocity.z = desired_velocity.z
 
 func get_input_vec():
-	var input_vec = Vector3(0, velocity.y, 0)
+	var input_vec = Vector3()
 
 	var camera_z_basis = camera.global_transform.basis.z
 	var camera_x_basis = camera.global_transform.basis.x
