0d4f69170bd006e487450d93bf359eedef7f10fe
theowiik <theo.le.wiik@gmail.com>
2019-08-30 10:01:09 +0200

    added projectile variable to projectile shooter and som basic script

--- src/objects/projectile_shooter/projectile_shooter.gd
+++ src/objects/projectile_shooter/projectile_shooter.gd
@@ -1,5 +1,8 @@
 extends Spatial
 
+const PROJECTILE = preload('res://src/objects/projectile/Projectile.tscn')
+onready var projectile_output_point = $ProjectileShooterMesh/ProjectileOutputPoint
+
 var damage = 1
 var mag_size = 30
 
@@ -9,5 +12,11 @@ func _ready():
 func reload():
 	pass
 
+func _unhandled_input(event):
+	if event.is_action_pressed('shoot'):
+		shoot()
+
 func shoot():
+	var b = PROJECTILE.instance()
+	b.start(Vector3(1, 1, 1))
 	pass
