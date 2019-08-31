508084bd286799b5f18c92e487d117f3aa01aa27
Theo Wiik <theo.le.wiik@gmail.com>
2019-08-31 13:19:07 +0200

    coins play sound on pickup

--- src/objects/coin/coin.gd
+++ src/objects/coin/coin.gd
@@ -3,6 +3,15 @@ extends Area
 var value = 1
 
 func _on_Coin_body_entered(body):
-	queue_free()
 	if body.has_method('take_coins'):
 		body.take_coins(value)
+
+	$AudioStreamPlayer3D.play()
+	hide()
+
+func hide():
+	$Hitbox.queue_free()
+	$"coinGold(Clone)".queue_free()
+
+func _on_AudioStreamPlayer3D_finished():
+	queue_free()
