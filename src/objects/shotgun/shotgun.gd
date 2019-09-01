5c7231639db2cd9d5424e5acf518e184d3022513
theowiik <theo.le.wiik@gmail.com>
2019-09-01 16:58:16 +0200

    shotguns bullet spread is "random" within a range

--- src/objects/shotgun/shotgun.gd
+++ src/objects/shotgun/shotgun.gd
@@ -1,7 +1,7 @@
 extends 'res://src/objects/projectile_shooter/projectile_shooter.gd'
 
-var bullets_per_shot = 5
-var bullet_spread = 0.5
+var bullets_per_shot = 50
+var max_bullet_spread = 0.5
 var rand = RandomNumberGenerator.new()
 
 func _init():
@@ -17,6 +17,7 @@ func _shoot():
 
 	for i in bullets_per_shot:
 		var rotation = rand.randi_range(0, 360)
+		var bullet_spread = rand.randf_range(0, max_bullet_spread)
 		var transformed_vec = Vector3(direction.x, direction.y + bullet_spread, direction.z)
 		transformed_vec = transformed_vec.rotated(forward.normalized(), rotation)
 		append_projectile(null, transformed_vec)
